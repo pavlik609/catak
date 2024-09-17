dcca6b840556b0a8ee1d81cfdf21e33bd0852c6e
pavlik609 <63738015+pavlik609@users.noreply.github.com>
2024-09-17 17:17:09 +0200

    Compiled static

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 windres rescources.rc -O coff -o icon.o
-gcc main.c icon.o -o catak -lraylib -lgdi32 -lglfw3 -lwinmm
+gcc main.c icon.o -o catak -lraylib -lgdi32 -lglfw3 -lwinmm -static
 "catak.exe"
--- comp.bat
+++ comp.bat
@@ -1,2 +1,2 @@
-gcc main.c icon.o -o catak -lraylib -lgdi32 -lglfw3 -lwinmm
+gcc main.c icon.o -o catak -lraylib -lgdi32 -lglfw3 -lwinmm -static
 "catak.exe"
